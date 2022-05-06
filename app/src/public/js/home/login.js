c03a2f8b67d2f40fc902cf27df3e010052d74509
skeleton2010 <93232412+skeleton2010@users.noreply.github.com>
2022-05-06 15:47:17 +0900

    catch로 에러잡기구현 및 로그인기능구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -21,6 +21,13 @@ function login() {
     })
     .then((res) => res.json())
     .then((res) => {
-
+        if (res.success) {
+            location.href = "/";
+        } else {
+            alert(res.msg);
+        }
+    })
+    .catch((err) => {
+        console.error(new Error("로그인중 에러발생"));
     });
 }
--- app/src/routes/home/home.crtl.js
+++ app/src/routes/home/home.crtl.js
@@ -24,13 +24,13 @@ const process = {
             const idx = users.id.indexOf(id);
             if (users.pw[idx] === pw) {
                 return res.json({
-                    succees: true,
+                    success: true,
                 });
             }
         }
 
         return res.json({
-            succees: false,
+            success: false,
             msg: "로그인에 실패하셨습니다"
         });
     },
