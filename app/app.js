d265a02df067768ea611374b34edec43d6055959
skeleton2010 <93232412+skeleton2010@users.noreply.github.com>
2022-05-06 13:06:40 +0900

    API 구성

--- app/app.js
+++ app/app.js
@@ -2,14 +2,17 @@
 
 const express = require("express");
 const app = express();
+const BodyParser = require("body-parser");
 
 const home = require("./src/routes/home");
 
 //앱셋팅
-app.set("views", "./src/views");
+app.set("views", "./src/views");    
 app.set("view engine", "ejs");
-
 app.use(express.static(`${__dirname}/src/public`));
+app.use(express.json());
+app.use(express.urlencoded({ extends: true}));
+
 app.use("/", home);
 
 module.exports = app;
