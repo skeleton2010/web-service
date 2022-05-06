8408cdf398f81883d146124ad0db62dee783b9ac
skeleton2010 <93232412+skeleton2010@users.noreply.github.com>
2022-05-06 13:35:05 +0900

    backend에서 로그인구현

--- app/src/routes/home/home.crtl.js
+++ app/src/routes/home/home.crtl.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const users = {
+    id: ["skeleton","admin", "guest"],
+    pw: ["skeleton", "admin", "guest"],
+};
+
 const output = {
     hello: (req, res) => {
         res.render("home/index");
@@ -12,8 +17,23 @@ const output = {
 
 const process = {
     login: (req, res) => {
-        console.log(req.body);
-    }
+        const id = req.body.id,
+            pw = req.body.pw;
+
+        if(users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.pw[idx] === pw) {
+                return res.json({
+                    succees: true,
+                });
+            }
+        }
+
+        return res.json({
+            succees: false,
+            msg: "로그인에 실패하셨습니다"
+        });
+    },
 };
 
 module.exports = {
